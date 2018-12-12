f48a11b265c502fdcc4a661c265946b9bd5d3dce
alx <acriptis@gmail.com>
2018-12-12 17:34:41 +0300

    add telegram channel

---
+++ hello_bot/scripts/start_telegram.py
@@ -0,0 +1,61 @@
+from telegram.ext import Updater
+from telegram.ext import CommandHandler, MessageHandler, Filters
+################# Universal Import ###################################################
+import sys
+import os
+SELF_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(SELF_DIR)
+PREROOT_DIR = os.path.dirname(ROOT_DIR)
+print(ROOT_DIR)
+sys.path.append(ROOT_DIR)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bank_bot.settings")
+# #####################################################
+import django
+django.setup()
+
+from components.skills.bank_consult_skill import BankConsulterSkill, AgentSkillInitializer, WeatherSkill, AlarmSkill
+from root_skill.root_skill import RootSkill
+
+if __name__ == '__main__':
+    """
+    Fast bootstrapped script to make telegram bridge between Agent and Telegram
+    You need to export environmental variable TG_BOT_TOKEN with your Telegram token before start
+    """
+
+    humans = {}
+
+    def init_agent():
+        return AgentSkillInitializer([BankConsulterSkill, WeatherSkill, AlarmSkill, RootSkill])
+
+    def start(bot, update):
+        chat_id = update.message.chat_id
+        humans[chat_id] = init_agent()
+        bot.send_message(chat_id=chat_id, text='Добрый день, человек. В чём ваша проблема?')
+
+    def user_client(bot, update):
+
+        chat_id = update.message.chat_id
+        user_msg = update.message.text
+        print('{} >>> {}'.format(chat_id, user_msg))
+        if chat_id not in humans:
+            humans[chat_id] = init_agent()
+
+        agent = humans[chat_id]
+
+        bot_resp = agent(user_msg)
+        one_resp = bot_resp[0]
+        print('{} <<< {}'.format(chat_id, one_resp))
+
+        bot.send_message(chat_id=chat_id, text=one_resp)
+
+
+    updater = Updater(token=os.environ['TG_BOT_TOKEN'])
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    msg_handler = MessageHandler(Filters.text, user_client)
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(msg_handler)
+
+    updater.start_polling()
+    updater.idle()
