dceda2fc3ba58601a981f065fc4ccd62fbcd30bf
alx <acriptis@gmail.com>
2018-12-24 15:11:20 +0300

    add admin interfaces

--- hello_bot/interactions/admin.py
+++ hello_bot/interactions/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from interactions.models.interactions import Interaction
+from interactions.models.user_interaction import UserInteraction
+from interactions.models.user_slot_process import UserSlotProcess
+from interactions.models.user_slot import UserSlot
+from interactions.models.userdialog import UserDialog
 
-# Register your models here.
+
+class InteractionAdmin(admin.ModelAdmin):
+
+    pass
+admin.site.register(Interaction, InteractionAdmin)
+
+class UserInteractionAdmin(admin.ModelAdmin):
+    # list_display = ('__str__', 'interaction', 'state')
+    list_display = ('interaction', 'state')
+
+admin.site.register(UserInteraction, UserInteractionAdmin)
+
+
+class UserSlotProcessAdmin(admin.ModelAdmin):
+    list_display = ('user', 'slot_codename', 'state')
+    pass
+admin.site.register(UserSlotProcess, UserSlotProcessAdmin)
+
+
+class UserSlotAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(UserSlot, UserSlotAdmin)
+
+
+class UserDialogAdmin(admin.ModelAdmin):
+    # list_display = ('__str__', 'interaction', 'state')
+    readonly_fields = ('serialize_dialog_dirty',)
+    # readonly_fields = ('__str__', 'serialize_dialog_dirty',)
+
+    # fields = ('__str__', 'serialize_dialog_dirty', )
+    fields = ('serialize_dialog_dirty', )
+
+admin.site.register(UserDialog, UserDialogAdmin)
