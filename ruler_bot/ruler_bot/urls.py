b91eec3751cfa98dca90a5fc7b5fd3491a4436fe
alx <acriptis@gmail.com>
2019-03-20 15:52:14 +0300

    clean urls

--- ruler_bot/ruler_bot/urls.py
+++ ruler_bot/ruler_bot/urls.py
@@ -19,23 +19,11 @@ from django.urls import include, path
 
 from dp_assistant_rest.skill_interactor_api_view import SkillInteractor
 
-# from rest_framework import routers
-# from quickstart import views
-# from persona import views as persona_views
-#
-# router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
-# router.register(r'personas', persona_views.PersonaViewSet)
-# from django_mongoengine import mongo_admin
-
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('ruler_call/', SkillInteractor.as_view()),
     path('admin/', admin.site.urls),
-    # path('madmin/', mongo_admin.site.urls),
+
 
 ]
