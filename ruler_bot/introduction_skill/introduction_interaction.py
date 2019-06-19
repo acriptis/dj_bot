b516c303c38895935d70cc2d7d3bf0c723923526
alx <acriptis@gmail.com>
2019-06-19 12:41:59 +0300

    clean code

--- ruler_bot/introduction_skill/introduction_interaction.py
+++ ruler_bot/introduction_skill/introduction_interaction.py
@@ -8,9 +8,6 @@ class IntroductionInteraction(Interaction):
         ### Slots Declaration #####################################################################
         # declarations which are agnostic to UserDomain:
         slots_factory = SlotsFactory()
-
-        # self.username_slot = slots_factory.produce_free_text_slot('username_slot', "Как вас зовут?")
-
         self.username_slot = slots_factory.produce_username_text_slot(
             'username_slot', "Дружок, как тебя зовут?")
 
@@ -29,7 +26,6 @@ class IntroductionInteraction(Interaction):
                                                                      })
 
         slots = [self.username_slot, self.interests_slot]
-        # slots = [self.interests_slot, username_slot]
         name = "PrivateInfoSlottyForm"
         self.sfi = SlottyFormInteraction.make_slottfy_form(name, slots)
 
@@ -49,41 +45,21 @@ class IntroductionInteraction(Interaction):
         self.sfi.connect_to_dataflow(udc)
 
         udc.im.register_interaction(self.sfi)
-        #
         # ############### Prepare RECEPTOR #################################################
-        # # this Interaction may be activated by Receptor (actually it is binary intent classifier here)
-        # self.global_trigger_receptor = PhrasesMatcher(phrases=["ПОЗНАКОМИМСЯ", "ЗНАКОМСТВО"],
-        #                                               daemon_if_matched=self.sfi.start)
-        # # TODO Receptor has intrification of connection. Is it BAD? How to restore it?
-        #
         # ##################### System Connection (must be redone on system restart) #############
-        # # connect receptor:
-        # udc.receptors_pool.connect(self.global_trigger_receptor)
-        # # self.ic.user_message_signal.connect(self.global_trigger_receptor, weak=False)
-        # self.ic.DialogPlanner.sendText("Добро пожаловать в мир глубокого обучения!")
-
         from components.receptors.models import Receptor
-        # from components.signal_reflex_routes.models.signals import ReceptorTriggeredSignal
+        from components.signal_pattern_reflex.signal_pattern import SignalPattern
         receptor, created = Receptor.get_or_create(
             class_name='PhrasesMatcher', init_args={'phrases': ["ПОЗНАКОМИМСЯ", "ЗНАКОМСТВО"]})
 
-
-        #udc.user_domain.user_message_signal.connect_receptor(receptor)
-        from components.signal_pattern_reflex.signal_pattern import SignalPattern
         sp, _ = SignalPattern.get_or_create_strict(signal_type="UserMessageSignal")
         udc.user_domain.user_message_signal_pattern.connect(receptor.__call__)
-        # udc.user_domain.user_message_signal_pattern.connect(receptor.__call__)
-        from components.signal_pattern_reflex.signal import Signal
-        from components.signal_pattern_reflex.signal_pattern import SignalPattern
         receptor_trigger_signal_pattern, _ = SignalPattern.get_or_create_strict(
             signal_type="ReceptorTriggeredSignal", receptor=receptor)
-        # import ipdb; ipdb.set_trace()
-        #receptor_trigger_signal.connect_object_method(instance_locator=self, method_name='start')
         receptor_trigger_signal_pattern.connect(self.start)
 
     def start(self, *args, **kwargs):
         # inits UserInteraction
-        #import ipdb; ipdb.set_trace()
         super().start(*args, **kwargs)
         # load context:
         self.ic = kwargs['user_domain'].get_user_domain_controller()
