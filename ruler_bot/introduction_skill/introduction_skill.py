7499400afaae02293ee4432660728e89ff4da38c
alx <acriptis@gmail.com>
2019-06-19 12:42:51 +0300

    clean introduction skill

--- ruler_bot/introduction_skill/introduction_skill.py
+++ ruler_bot/introduction_skill/introduction_skill.py
@@ -19,12 +19,4 @@ class IntroductionSkill():
 
         # base interactions and scenario linking:
         self.intro_int, _ = IntroductionInteraction.get_or_create()
-
-        #self.udc.im.register_interaction(self.intro_int)
         self.intro_int.connect_to_dataflow(self.udc)
-
-    # def process_step(self, message_str, user_id, state_obj):
-    #     print("PROCESS STEP in Introduction Skill")
-    #     import ipdb; ipdb.set_trace()
-    #     print("PROCESS STEP in Introduction Skill")
-    #     # super().process_step(message_str, user_id, state_obj)
